dc90f155729ebfb55f7e45e37c1984006ade4ded
Thangella Aravind <aravindthangella1997@gmail.com>
2025-09-18 23:31:43 +0530

    hello script

---
+++ 02-varaibles.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Aravind:: Hello Ram, how are you?"
+echo "Ram:: I am good, how about you?"
+echo "Aravind:: I am also good, how is your project going?"
+echo "Ram:: It's going well!"
+
