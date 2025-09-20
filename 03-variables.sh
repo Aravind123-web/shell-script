d7dcb1337790658ed194f947bda7a870d4ab4b9c
Thangella Aravind <aravindthangella1997@gmail.com>
2025-09-20 23:14:30 +0530

    hello script

---
+++ 03-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+person1=Aravind # No space between = and value
+person2=ram
+echo "$person1:: Hello $person2 , How are you?"
+echo "$person2:: I am Good, How about uou?"
+echo "$person1:: I am also good, How it is going on your project"
+echo "$person2:: It is going on well"
