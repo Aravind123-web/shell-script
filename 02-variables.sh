d1c04a4634677b7c7ba1d5ece8df1e9f8f912f9d
Thangella Aravind <aravindthangella1997@gmail.com>
2025-09-26 13:03:45 +0530

    hello script

---
+++ 02-variables.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Aravind:: Hello ram , How are you?"
+echo "ram:: I am Good, How about uou?"
+echo "Aravind:: I am also good, How it is going on your project"
+echo "ram:: It is going on well"
