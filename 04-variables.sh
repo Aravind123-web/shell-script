f07daf394c66cd8072b9794dcffd40d06a9631af
Thangella Aravind <aravindthangella1997@gmail.com>
2025-09-28 22:11:07 +0530

    hello script

---
+++ 04-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#declaring variable
+person1=$1
+person2=$2
+
+#referring variable
+echo "$person1:: Hello $person2 , How are you?"
+echo "$person2:: I am Good, How about uou?"
+echo "$person1:: I am also good, How it is going on your project"
+echo "$person2:: It is going on well"
