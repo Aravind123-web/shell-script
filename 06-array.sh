10a8671da4331cd3d87878e383134d5f17de7910
Thangella Aravind <aravindthangella1997@gmail.com>
2025-09-28 23:05:48 +0530

    hello script

--- 06-array.sh
+++ 06-array.sh
@@ -8,7 +8,7 @@ MOVIES=("RRR" "OG" "KANTARA CHAPTER-1" "KGF CHAPTER-2")
 
 #list always start with 0.
 echo "First movie is: ${MOVIES[0]"
-ECHO "Second movie is: ${MOVIES[1]}"
-ECHO "Second movie is: ${MOVIES[2]}"
-ECHO "Second movie is: ${MOVIES[3]}"
-ECHO "Second movie is: ${MOVIES[@]}" # PRINT ALL VALUES FROM ARRAY
+echo "Second movie is: ${MOVIES[1]}"
+echo "Second movie is: ${MOVIES[2]}"
+echo "Second movie is: ${MOVIES[3]}"
+echo "Second movie is: ${MOVIES[@]}" # PRINT ALL VALUES FROM ARRAY
