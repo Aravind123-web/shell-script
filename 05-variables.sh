bfcc4901721c6339566cb349982bd1b89dbdd4a2
Thangella Aravind <aravindthangella1997@gmail.com>
2025-09-28 22:35:26 +0530

    hello script

---
+++ 05-variables.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+echo "Please enter username::"
+
+read USERNAME #HERE USERNAME IS VARIABLE
+
+echo "Please enter password::"
+
+read PASSWORD
+
+ECHO "Username is: $USERNAME, Password is: $PASSWORD
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
