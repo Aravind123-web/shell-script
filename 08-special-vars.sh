33051c58bec083e27fb4255a723487dfb8f0cae1
Thangella Aravind <aravindthangella1997@gmail.com>
2025-10-05 13:00:52 +0530

    Hello-script

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -3,3 +3,5 @@
 echo "All variables: $@" # If you want to print all variables 
 echo "Number of variables passed: $#" # How many variables passed to script
 echo "Script Name: $0" # If you want to know script name
+echo "Current working directory: $PWD" # It prints curent working directory
+
