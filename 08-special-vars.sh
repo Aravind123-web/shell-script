a5e90d687fefdff2c45ff50ec2784fb75bcdeae4
Thangella Aravind <aravindthangella1997@gmail.com>
2025-10-05 12:45:39 +0530

    Hello-script

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "All variables: $@"
-
-echo "Number of variables passed: $#"
+echo "All variables: $@" # If you want to print all variables 
+echo "Number of variables passed: $#" # How many variables passed to script
+echo "Script Name: $0" # If you want to know script name
