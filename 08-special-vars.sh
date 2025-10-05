23131932e03aa93c7f21c4b61393590a26b2d514
Thangella Aravind <aravindthangella1997@gmail.com>
2025-10-05 13:14:28 +0530

    Hello-script

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -5,4 +5,4 @@ echo "Number of variables passed: $#" # How many variables passed to script
 echo "Script Name: $0" # If you want to know script name
 echo "Current working directory: $PWD" # It prints curent working directory
 echo "Home directory of current user: $HOME"
-ECHO "Which user running this script: $USER"
+echo "Which user running this script: $USER"
