65ff210dca434594775663c55220ac74de5f7c8f
Thangella Aravind <aravindthangella1997@gmail.com>
2025-11-24 11:55:31 +0530

    remove bug

--- 10-installation.sh
+++ 10-installation.sh
@@ -4,10 +4,14 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ] 
 then
     echo "Please run this script with root access."
-    exit 1
+    exit 1  # Manually exit if error comes
 else 
      echo "you are super user."
 fi
 dnf install mysql -y
 
+if [ $? -ne 0 ]
+then 
+      echo "Installation of MySql is Failure"
+fi
 echo "is Script Procedding?"
