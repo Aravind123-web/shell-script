e7de2299ae09208826412e2333f53da7ebf29cd5
Thangella Aravind <aravindthangella1997@gmail.com>
2025-11-24 22:27:27 +0530

    fix errors

---
+++ 11-functions.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+USERID=$(id -u)
+#DECLARTION OF FUNCTION SYNTAX
+VALIDATE() {
+
+    echo "exit statue: $1"
+    echo "What are you doing: $2"
+
+}
+
+
+if [ $USERID -ne 0 ] 
+then
+    echo "Please run this script with root access."
+    exit 1  # Manually exit if error comes
+else 
+     echo "you are super user."
+fi
+dnf install git -y
+VALIDATE $? "Installing git"
+
+dnf install nginx -y
+VALIDATE $? "Installing nginx"
