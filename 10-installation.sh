db498f9eb25db6d02e079a53821b44fe8ca2d02c
Thangella Aravind <aravindthangella1997@gmail.com>
2025-11-24 10:31:37 +0530

    remove bug

--- 10-installation.sh
+++ 10-installation.sh
@@ -3,7 +3,7 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ] 
 then
-    echo "Please run tis script with root access."
+    echo "Please run this script with root access."
     
 else 
      echo "you are super user."
