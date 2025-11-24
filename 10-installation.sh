444c9d23954e743d42076cdcdf6d7b14c0d079a6
Thangella Aravind <aravindthangella1997@gmail.com>
2025-11-24 12:06:42 +0530

    remove bug

--- 10-installation.sh
+++ 10-installation.sh
@@ -14,8 +14,17 @@ if [ $? -ne 0 ]
 then 
       echo "Installation of git is Failure"
       exit 23
+else  
+    echo  "My Installation of git is Success"      
 fi
 
 dnf install nginx -y
 
+if [ $? -ne 0]
+then 
+      echo "Installation of nginx is Failure"
+      exit 127    
+else  
+      echo "My Installation of nginx is Success"      
+fi
 echo "is Script Procedding?"
