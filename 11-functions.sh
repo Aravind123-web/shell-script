a0c8ef8943b62c544e436d0dd74f2b12f040a1d9
Thangella Aravind <aravindthangella1997@gmail.com>
2025-11-24 22:30:24 +0530

    fix errors

--- 11-functions.sh
+++ 11-functions.sh
@@ -12,7 +12,7 @@ VALIDATE() {
 if [ $USERID -ne 0 ] 
 then
     echo "Please run this script with root access."
-    exit 1  # Manually exit if error comes
+    #exit 1  # Manually exit if error comes
 else 
      echo "you are super user."
 fi
