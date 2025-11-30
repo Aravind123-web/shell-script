0c5b1b99420164acc4f2fcd5f19b5e95b6de2cb8
Thangella Aravind <aravindthangella1997@gmail.com>
2025-11-30 20:56:30 +0530

    fix error

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -5,6 +5,11 @@ TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
 if [ $USERID -ne 0 ]
 then
     echo "Please run this script as root user"
@@ -20,7 +25,7 @@ do
     dnf list installed $i &>>$LOGFILE
     if [ $? -eq 0 ]
     then 
-        echo "$i is already installed...SKIPPING"
+        echo -e "$i is already installed...$Y SKIPPING $N"
     else
         echo "$i is not installed...Need to Install"
     fi
